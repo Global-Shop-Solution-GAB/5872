59202e1309606c2b51557ef9b50a6564f1c66829
Service Web <sw@gssmail.com>
2024-03-15 09:00:17 -0500

    Commit changes in C:\Websites\Git\emarlow\5872\GCG_5872_Bom.sql

---
+++ GCG_5872_Bom.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "GCG_5872_Bom"(
+ "UserID" CHAR(8),
+ "Seq" INTEGER,
+ "Rec_Seq" CHAR(6),
+ "Position" VARCHAR(15),
+ "LevelChar" VARCHAR(8),
+ "Level" TINYINT,
+ "Parent" VARCHAR(20),
+ "Component" VARCHAR(20),
+ "Quantity_6" NUMERIC(12,4),
+ "Quantity" NUMERIC(12,4),
+ "Bom_Um" VARCHAR(3),
+ "Node" VARCHAR(200) );
